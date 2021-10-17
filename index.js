b8af2b8efeb7495f24cc6a46d4e042af6650cdfc
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-10-17 17:24:49 +0100

    changes

---
+++ index.js
@@ -0,0 +1,45 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is your name?",
+        name: "username",
+    },
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'Please write a short description of what your project is about ',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+      }, 
+   
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
