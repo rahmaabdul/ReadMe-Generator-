6dc8d4ea6209e0b2b4f36480b992fdd7b3354807
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-11-15 18:15:13 +0000

    corrections

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this applicatio
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: "input",
-        message: "What is your name?",
-        name: "username",
-    },
-    {
-        type: 'input',
-        name: 'Title',
-        message: 'What is your project title?',
-      },
-      {
-        type: 'input',
-        name: 'Description',
-        message: 'Please write a short description of what your project is about',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username',
-      },
-      {
-        type: 'input',
-        name: 'linkedin',
-        message: 'Enter your LinkedIn URL.',
-      }, 
-      
+const questions = [7yu
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "username",
+  },
+  {
+    type: "input",
+    name: "Title",
+    message: "What is your project title?",
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: "Please write a short description of what your project is about",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub Username",
+  },
+  {
+    type: "input",
+    name: "linkedin",
+    message: "Enter your LinkedIn URL.",
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  Fs.writefile(filename, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success! The README.md has been generated");
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
