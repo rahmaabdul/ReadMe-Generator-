f08d8a25d89f63c59209d30a5eb7fc987ff558e3
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-11-15 18:15:39 +0000

    corrections

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [7yu
+const questions = 
   {
     type: "input",
     message: "What is your name?",
